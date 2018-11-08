1695201054728ee1ee73b1bcf4a5ce5f11feb8e6
Joshua Tan <jtsarvida@gmail.com>
2018-11-08 11:01:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,15 @@ class Student
     DB[:conn].execute(sql)
   end
   
-
+  def self.all_students_in_grade_9
+  end
+  
+  def self.students_below_12th_grade
+  end
+  
+  def self.first_X_students_in_grade_10
+  end 
+  
+  def self.first_student_in_grade_10
       
 end
